a0b97cd484567335de73a5ce36e33758cbe1319f
pavlo_sadivnychyi <pavlo.sadivnychyi@devlight.io>
2026-01-06 18:29:52 +0200

    fifth commit

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
+  // const app = await NestFactory.create(AppModule, {cors: true});
+  const app = await NestFactory.create(AppModule);
 
-  // app.enableCors({
-  //   origin: 'http://localhost:3001', // твій фронт
-  //   credentials: true,
-  // });
+  app.enableCors({
+    origin: 'http://localhost:3001', // твій фронт
+    credentials: true,
+  });
 
   await app.listen(3000);
 }
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,26 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService extends PrismaClient<Prisma.PrismaClientOptions, Prisma.LogLevel> implements OnModuleInit {
+    constructor() {
+        super({
+            log: [
+                { emit: 'event', level: 'query' },
+                { emit: 'event', level: 'error' },
+                { emit: 'event', level: 'info' },
+                { emit: 'event', level: 'warn' },
+            ],
+        });
+
+        this.$on('error', e => console.log(e.message));
+        this.$on('warn', e => console.log(e.message));
+        this.$on('info', e => console.log(e.message));
+        this.$on('query', e => {
+            console.log(`[${e.duration} ms] ${e.query}; ${e.params}`);
+        });
+    }
+
     async onModuleInit() {
         await this.$connect();
     }
