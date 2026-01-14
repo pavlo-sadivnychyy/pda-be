c8d4fb290c4671b3d68395f3ab06a75fa5c783ce
pavlo_sadivnychyi <pavlo.sadivnychyi@devlight.io>
2026-01-14 12:11:35 +0200

    added CORS

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,31 @@ import { AppModule } from './app.module';
 import * as process from 'node:process';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule, {cors: true});
   const app = await NestFactory.create(AppModule);
 
+  const allowedOrigins = [
+    'http://localhost:3001', // локальний фронт (якщо використовуєш)
+    'http://localhost:3000', // Next dev
+    'https://pda-fe-dev-3883128e3ffd.herokuapp.com', // Heroku фронт
+  ];
+
   app.enableCors({
-    origin: 'http://localhost:3001', // твій фронт
+    origin: (origin, callback) => {
+      // дозволяємо запити без Origin (наприклад Postman, server-to-server)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error('Not allowed by CORS'), false);
+    },
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
   await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap().catch((error) => console.log(error));
